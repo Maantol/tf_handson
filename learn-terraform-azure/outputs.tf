b28c07543b22eefbceee8883fd01509ca85a7a79
Matti Tolonen <mattitolonen@Matti-Mac-mini.local>
2025-04-29 19:35:50 +0300

    updated azure output

--- learn-terraform-azure/outputs.tf
+++ learn-terraform-azure/outputs.tf
@@ -1,16 +1,21 @@
 output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
+  description = "The ID of the resource group"
+  value       = azurerm_resource_group.rg.id
 }
 
 output "vnet_id" {
-  value = azurerm_virtual_network.vnet.id
+  description = "The ID of the virtual network"
+  value       = azurerm_virtual_network.vnet.id
 }
 output "vnet_address_space" {
-  value = azurerm_virtual_network.vnet.address_space
+  description = "The address space of the virtual network"
+  value       = azurerm_virtual_network.vnet.address_space
 }
 output "vnet_location" {
-  value = azurerm_virtual_network.vnet.location
+  description = "The location of the virtual network"
+  value       = azurerm_virtual_network.vnet.location
 }
 output "vnet_name" {
-  value = azurerm_virtual_network.vnet.name
+  description = "The name of the virtual network"
+  value       = azurerm_virtual_network.vnet.name
 }
